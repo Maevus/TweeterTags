3f7706a84df2f5a72d6853a314954a60fb00eeb6
Maevus <maevus@gmail.com>
2019-03-09 09:23:30 +0000

    first commit - working P1

---
+++ TweeterTags/TweetsTVC.swift
@@ -0,0 +1,118 @@
+//
+//  TweetsTVC.swift
+//  TweeterTags
+//
+//  Created by Maeve Lynskey on 06/03/2019.
+//  Copyright © 2019 COMP47390. All rights reserved.
+//
+
+import UIKit
+
+class TweetsTVC: UITableViewController {
+    
+    var twitterQueryText: String? = "#UCD"  {
+        didSet {
+             // code here?
+            }
+    }
+    
+    var tweets = [TwitterTweet]() {
+        didSet {
+            if tableView.window != nil {
+                tableView.reloadData()
+            }
+            print ("\(tweets.count))")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        refresh()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    
+    func refresh() {
+        let request = TwitterRequest(search: twitterQueryText!, count: 8)
+                request.fetchTweets { (tweets) -> Void in
+                    DispatchQueue.main.async { () -> Void in
+                        self.tweets = tweets }
+                    }
+    }
+    
+    // MARK: - Table view data source
+    
+    //    override func numberOfSections(in tableView: UITableView) -> Int {
+    //        return 1
+    //    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        print("numberofrows:: \(tweets.count)")
+        return tweets.count
+        
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Tweet", for: indexPath)
+        let tweeter = self.tweets[indexPath.row]
+        
+        cell.textLabel?.text = tweeter.text
+        
+        return cell
+    }
+    
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+
