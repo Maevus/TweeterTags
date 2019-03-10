36b17699560282a93c90ed78784935180b6e3fa2
Maevus <maevus@gmail.com>
2019-03-10 16:47:38 +0000

    working disappearing headers

--- TweeterTags/TweetDetailTVC.swift
+++ TweeterTags/TweetDetailTVC.swift
@@ -33,18 +33,13 @@ class TweetDetailTVC: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
+        return SectionTypes.allCases.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -70,6 +65,31 @@ class TweetDetailTVC: UITableViewController {
                 return "Users"
             }
         }
+    
+    /*
+     Hide unused headers by minimising them when they are not needed.
+    */
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        var height: CGFloat = 21
+
+        switch getSection(forSection: section) {
+        case .hashtags:
+            if hashtags.count < 1 {
+                height = CGFloat.leastNonzeroMagnitude
+            }
+        case .links:
+            if urls.count < 1 {
+                height = CGFloat.leastNonzeroMagnitude
+            }
+        case .users:
+            if userMentions.count < 1 {
+                height = CGFloat.leastNonzeroMagnitude
+            }
+        }
+        
+        return height
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "tweetDetailsCell", for: indexPath) as! TweetDetailTableViewCell
@@ -98,52 +118,13 @@ class TweetDetailTVC: UITableViewController {
         return cell
     }
     
- 
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    //override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-    }
-    */
+   // }
+  //  */
 
 }
